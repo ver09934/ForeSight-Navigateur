abd53127d5362479b69557308dc35fb2d4037692
ver09934 <ianvernooy@gmail.com>
2018-02-22 12:54:19 -0500

    Work on location updates

--- app/src/main/java/com/foresight/navigateur/MapsActivity.java
+++ app/src/main/java/com/foresight/navigateur/MapsActivity.java
@@ -10,7 +10,9 @@ import android.support.v4.content.ContextCompat;
 import android.view.View;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,7 +29,13 @@ import com.google.android.gms.tasks.OnSuccessListener;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleMap mMap;
+
     private FusedLocationProviderClient mFusedLocationClient;
+
+    private LocationRequest mLocationRequest;
+
+    private LocationCallback mLocationCallback;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        /*
+        startLocationUpdates();
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                    addMarkerAndZoomTo(location);
+                }
+            }
+        };
+        */
 
     }
 
@@ -55,23 +78,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-
         // Show blue location dot and thingy to bring you there
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
         }
 
-        functionOne(getCurrentFocus());
-        createLocationRequest();
+        //functionOne(getCurrentFocus());
+
+        setupLocationRequest();
+
+        startLocationUpdates();
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                    addMarkerAndZoomTo(location);
+                }
+            }
+        };
     }
 
-    protected void createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
+    protected void setupLocationRequest() {
+        mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(10000);
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
+
+    /*
+    //So that location results are not received when the app is not active
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+    */
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null /* Looper */);
+    }
+
+    //------------------------User Functions------------------------------
 
     Marker testMarker = null;
     // UPDATE CURRENT LOCATION MARKER
@@ -88,20 +139,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         if (testMarker != null)
                             testMarker.remove();
 
-                        LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-
-                        testMarker = mMap.addMarker(new MarkerOptions().position(myLatLng).title("Your Current Location"));
-                        CameraPosition currentLocationCameraPosition = new CameraPosition.Builder().target(myLatLng).zoom(17).build();
-
-                        functionTwo(getCurrentFocus());
-
-                        //mMap.animateCamera(CameraUpdateFactory.newCameraPosition(currentLocationCameraPosition), 3000, null);
-                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(currentLocationCameraPosition));
+                        testMarker = addMarkerAndZoomTo(location);
                     }
                 }
             });
 
         }
+    }
+
+    private Marker addMarkerAndZoomTo(Location inputLocation) {
+        Double lat = inputLocation.getLatitude();
+        Double lon = inputLocation.getLongitude();
+        LatLng myLatLng = new LatLng(lat, lon);
+        Marker markerOut = mMap.addMarker(new MarkerOptions().position(myLatLng).title(lat + ", " + lon)); // Label location with coordinates
+        CameraPosition currentLocationCameraPosition = new CameraPosition.Builder().target(myLatLng).zoom(17).build(); // Zoom 17 is pretty close, see maps api documentation
+        //mMap.animateCamera(CameraUpdateFactory.newCameraPosition(currentLocationCameraPosition), 3000, null);
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(currentLocationCameraPosition));
+        return markerOut;
     }
 
     public void functionTwo(View view) {    }
