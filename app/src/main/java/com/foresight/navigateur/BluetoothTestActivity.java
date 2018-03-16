53e808ecf49ce47a1f475be3c619cb8f332478e8
ver09934 <ianvernooy@gmail.com>
2018-03-16 18:01:41 -0400

    Add prelim bluetooth setup method

--- app/src/main/java/com/foresight/navigateur/BluetoothTestActivity.java
+++ app/src/main/java/com/foresight/navigateur/BluetoothTestActivity.java
@@ -1,7 +1,10 @@
 package com.foresight.navigateur;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class BluetoothTestActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class BluetoothTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_test);
+
+        setupBlueTooth();
+
+    }
+
+    public static final int REQUEST_ENABLE_BT = 1;
+
+    public void setupBlueTooth() {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Your device does not support bluetooth" + , Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
     }
 }
