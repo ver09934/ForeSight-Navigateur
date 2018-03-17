e626a6548aa164206e5f1a20bb6297e3d867403c
ver09934 <ianvernooy@gmail.com>
2018-03-17 18:27:12 -0400

    Work on list paired devices

--- app/src/main/java/com/foresight/navigateur/BluetoothTestActivity.java
+++ app/src/main/java/com/foresight/navigateur/BluetoothTestActivity.java
@@ -1,24 +1,39 @@
 package com.foresight.navigateur;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Set;
+
+/*
+Bluetooth setup and testing class.
+Contains a lot of code borrowed from the android developer guides,
+with a link available here:
+https://developer.android.com/guide/topics/connectivity/bluetooth.html
+*/
+
 public class BluetoothTestActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBluetoothAdapter;
+    private TextView mBluetoothTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_test);
 
+        mBluetoothTextView = (TextView) findViewById(R.id.bluetooth_text_view);
+
         enableBluetooth();
 
     }
-
-    public static final int REQUEST_ENABLE_BT = 1;
-    public BluetoothAdapter mBluetoothAdapter;
 
     public void enableBluetooth() {
 
@@ -38,5 +53,35 @@ public class BluetoothTestActivity extends AppCompatActivity {
     }
 
     //public void discoverDevices() {}
+
+    // Display known Devices
+    public void listPairedDevicesOnScreen(View view) {
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        String displayText = "Paired Devices: \n";
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                displayText += "Name: " + device.getName() + " ";
+                displayText += "Mac address: " + device.getAddress() + "\n"; // MAC address
+            }
+        }
+
+        mBluetoothTextView.setText(displayText);
+    }
+
+    //public void listDiscoveredDevices() {}
+
+    /*
+    Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+    if (pairedDevices.size() > 0) {
+        // There are paired devices. Get the name and address of each paired device.
+        for (BluetoothDevice device : pairedDevices) {
+            String deviceName = device.getName();
+            String deviceHardwareAddress = device.getAddress(); // MAC address
+        }
+    }
+    */
 
 }
