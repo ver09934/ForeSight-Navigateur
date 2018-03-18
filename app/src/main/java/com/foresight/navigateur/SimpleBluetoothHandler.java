016737f7ae1751ffb3fcfa396d3cf7185dfc79aa
ver09934 <ianvernooy@gmail.com>
2018-03-18 14:12:35 -0400

    Create test class

---
+++ app/src/main/java/com/foresight/navigateur/SimpleBluetoothHandler.java
@@ -0,0 +1,11 @@
+package com.foresight.navigateur;
+
+public class SimpleBluetoothHandler {
+
+    private final String DEVICE_ADDRESS="00:14:03:05:FF:E6";
+
+    public SimpleBluetoothHandler() {
+
+    }
+
+}
